161843847b611f6876859cbf307ec2eae15b9cc9
chelz2 <golirida@hotmail.com>
2024-12-26 23:18:15 +0530

    profile scroll app

---
+++ script.js
@@ -0,0 +1,43 @@
+const people = [
+  {
+    name: "Jamie Williams",
+    age: 26,
+    gender: "female",
+    location: "Los Angeles, CA",
+    imageURL: "https://randomuser.me/api/portraits/women/1.jpg",
+    looking: "Female looking for male",
+  },
+  {
+    name: "John Smith",
+    age: 35,
+    gender: "male",
+    location: "New York, NY",
+    imageURL: "https://randomuser.me/api/portraits/men/1.jpg",
+    looking: "Male looking for female",
+  },
+  {
+    name: "Bob Johnson",
+    age: 42,
+    gender: "male",
+    location: "Chicago, IL",
+    imageURL: "https://randomuser.me/api/portraits/men/2.jpg",
+    looking: "Male looking for male",
+  },
+  {
+    name: "Shannon Jackson",
+    age: 29,
+    gender: "female",
+    location: "Los Angeles, CA",
+    imageURL: "https://randomuser.me/api/portraits/women/2.jpg",
+    looking: "Female looking for female",
+  },
+];
+
+const container = document.querySelector(".container");
+const img ocument.querySelector("img");
+const container = document.querySelector(".profile-info");
+const container = document.querySelector("#next");
+
+img 
+profile
+nextButton
